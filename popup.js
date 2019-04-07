4af02518ede2da671b07488ba3157401179fb553
pashebor <pashebor@salesgenerator.pro>
2019-04-07 16:45:10 +0300

    first commit

---
+++ popup.js
@@ -0,0 +1,6 @@
+(() => {
+    const email = document.querySelector('input[name=email]'),
+        password = document.querySelector('input[name=password]'),
+        submitButton = document.querySelector('button[type=submit]');
+
+})();
