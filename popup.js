deab78ce3a56a6b6a2c5447df602ed1ebc1df899
pashebor <pashebor@salesgenerator.pro>
2019-05-10 18:24:07 +0300

    Done extension logic

--- popup.js
+++ popup.js
@@ -1,6 +1,35 @@
 (() => {
     const email = document.querySelector('input[name=email]'),
         password = document.querySelector('input[name=password]'),
-        submitButton = document.querySelector('button[type=submit]');
+        submitButton = document.querySelector('button[type=submit]'),
+        fieldsExsitense = !!(email && password && submitButton);
 
+        if(fieldsExsitense) {
+          chrome.storage.local.get(['auth'], (response) => {
+            const authData = response.auth;
+
+            if(!authData) {
+               submitButton.addEventListener('click', () => {
+                 const data = {
+                    email: email.value,
+                    password: password.value
+                  };
+
+                  setTimeout(() => {
+                    const alertText = document.querySelector('.popup p');
+
+                    if (alertText && alertText.innerText === 'Вы успешно авторизовались') {
+                      chrome.storage.local.set({ auth: data }, () => {
+                        console.log('[TODO logger] User data have been set: ', data);
+                      });
+                    }
+                  }, 1000);
+               })
+             } else {
+               email.value = authData.email;
+               password.value = authData.password;
+               submitButton.click();
+             }
+          });
+        }
 })();
